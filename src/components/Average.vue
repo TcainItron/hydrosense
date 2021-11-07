5f1c8aeb6b6be91f32256c02c3a4318c98416c59
Chevelle Boyer <chevelleboyer@gmail.com>
2021-11-07 14:10:08 -0700

    Add minutes and hours to dropdowns

--- src/components/Average.vue
+++ src/components/Average.vue
@@ -18,6 +18,24 @@
 	</v-col>
 </template>
 
+<script>
+	export default {
+		name: 'Average',
+		data () {
+			return {
+				endHour: null,
+				endMin: null,
+				hours: [...Array(24).keys()].map(x => x.toString().padStart(2, '0')),
+				mins: [...Array(60).keys()].map(x => x.toString().padStart(2, '0')),
+				rowCount: 0,
+				sliderValue: 1,
+				startHour: null,
+				startMin: null,
+			}
+		},
+	};
+</script>
+
 <style>
 	.avgs {
 		margin-top: 10%;
