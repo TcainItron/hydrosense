7e6c47c0ec475dce3995f8bd03f7981c0c095a34
Chevelle Boyer <chevelleboyer@gmail.com>
2021-11-07 14:14:19 -0700

    Use new average component in main app

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <v-main>
       <v-container>
         <v-row>
+          <Average/>
           <v-col cols="9">
             <v-row>
               <Slider
@@ -24,12 +25,14 @@
 </template>
 
 <script>
+import Average from './components/Average.vue';
 import Graph from './components/Graph';
 import Slider from './components/Slider.vue';
 
 export default {
   name: 'App',
   components: {
+    Average,
     Graph,
     Slider,
   },
