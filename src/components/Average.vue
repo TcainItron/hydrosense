cf7bf01537bc9c11d0fd691662bedbbca6056160
Chevelle Boyer <chevelleboyer@gmail.com>
2021-11-07 14:16:28 -0700

    Connect selected values to their models

--- src/components/Average.vue
+++ src/components/Average.vue
@@ -3,17 +3,17 @@
 		<h2 style='margin-left:20%;'>Graph an Average</h2>
 		<v-row>
 			<div style='margin-top:21%;margin-right:5%;width:15%;text-align:right;'>From:</div>
-			<v-select style='width:5%;margin-top:15%;' label="HH" :items="hours"></v-select>
+			<v-select v-model='startHour' style='width:5%;margin-top:15%;' label="HH" :items="hours"></v-select>
 
 			<div style="margin-top:20%;margin-left:2%;margin-right:2%;"> : </div>
-			<v-select style='width:5%;margin-top:15%;' label="mm" :items="mins"></v-select>
+			<v-select v-model='startMin' style='width:5%;margin-top:15%;' label="mm" :items="mins"></v-select>
 		</v-row>
 		<v-row>
 			<div style='margin-top:15%;margin-right:5%;width:15%;text-align:right;'>  To:</div>
-			<v-select style='width:5%;margin-top:10%;' label="HH" :items="hours"></v-select>
+			<v-select v-model='endHour' style='width:5%;margin-top:10%;' label="HH" :items="hours"></v-select>
 
 			<div style="margin-top:15%;margin-left:2%;margin-right:2%;"> : </div>
-			<v-select style='width:5%;margin-top:10%;' label="mm" :items="mins"></v-select>
+			<v-select v-model='endMin' style='width:5%;margin-top:10%;' label="mm" :items="mins"></v-select>
 		</v-row>
 	</v-col>
 </template>
